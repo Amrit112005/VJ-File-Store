94734d83e98e50c79261a7f25436fa627b1ad266
Amrit112005 <amrit2005pay@gmail.com>
2025-01-04 02:07:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "27778576"))
+API_HASH = environ.get("API_HASH", "fbe65bde59965ea035199d7c848ae3a4")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ce1723991756e48c35aa1.jpg')).split() # Bot Start Picture
